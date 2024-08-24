bfadf341f93d31bba11d6104acb4ec325944f0fb
ilzat <ilzat.sadykov.2006@bk.ru>
2024-08-24 15:38:37 +0600

    'скачал библиотеки И настроил store.ts'

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { Provider } from 'react-redux';
+import { store } from './app/store.ts';
+import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <CssBaseline/>
     <App />
-  </StrictMode>,
+    </BrowserRouter>
+  </Provider>
 )
